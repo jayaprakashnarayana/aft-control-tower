3b5212687e55ee6231bae2dc8738925390d9a6d9
Sahil Bansal <sahil2311@gmail.com>
2022-05-05 01:50:44 +0530

    updated state file

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "aft" {
-  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory?ref=1.3.5"
   ct_management_account_id    = var.ct_management_account_id
   log_archive_account_id      = var.log_archive_account_id
   audit_account_id            = var.audit_account_id
